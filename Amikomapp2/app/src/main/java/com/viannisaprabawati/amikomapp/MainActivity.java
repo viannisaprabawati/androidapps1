f89f4ac26f084e685ec10f14b60b1f85c6608da8
viannisaprabawati <viannisa1811@gmail.com>
2017-09-29 10:31:45 +0700

    Splash

---
+++ Amikomapp2/app/src/main/java/com/viannisaprabawati/amikomapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.viannisaprabawati.amikomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String msg = "--Amikom app :" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+        protected void onStart() {
+            super.onStart();
+            Log.d(msg, "The onStart() event");
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(msg, "The onRestart() event");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
+
+}
